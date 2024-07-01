75b5a25b15358c1b7499d01270d9f30b19e7477f
scarlat-trainer <78.cristi@gmail.com>
2024-07-01 17:28:29 +0300

    add multiple inputs in vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-
+import { resolve } from 'path'
 
 export default {
   // config options
-  base: "/vite-vanilla-puzzle-game/"
+  base: "/vite-vanilla-puzzle-game/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        help: resolve(__dirname, 'help.html'),
+      },
+    },
+  },
 }
